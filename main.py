589613841e0e4d46a5200d94f554ed6a99d3dfdc
rafi4478 <rafi.kazla@gmail.com>
2025-05-11 08:02:37 +0600

    third

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ print(len(name))
 # problem 2
 name = "Hello there"
 print(type(name))
+
+# problem 3
+def find_elder(age1,age2):
+    if age1 > age2:
+        return f"First brother is older ages {age1}"
+    else:
+        return f"Second brother is older ages {age2}"
+
+print(find_elder(12,13))
